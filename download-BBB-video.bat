a2e26c341b5b910dd08eef6bd83bfdd67f36633c
CrenshawCoding <anonyesmaybe@gmail.com>
2020-11-07 15:59:38 +0100

    remove pause

--- download-BBB-video.bat
+++ download-BBB-video.bat
@@ -12,5 +12,4 @@ youtube-dl -i %1 -o "%curDir%\presentation%~x1"
 youtube-dl -i %2 -o "%curDir%\webcam%~x2"
 ffmpeg -i "webcam%~x2" "webcam-audio.mp3"
 ffmpeg -i "presentation%~x1" -i "webcam-audio.mp3" "output.mp4"
-pause
 exit 0
