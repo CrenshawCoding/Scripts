95c003a2d5e01ceccd1cfff13a6012e9b8a3f7fd
CrenshawCoding <anonyesmaybe@gmail.com>
2020-12-13 17:24:08 +0100

    resetting

--- public/make_whatsapp_video.bat
+++
@@ -1,3 +0,0 @@
-rem // converts a video into a format thats sendable by whatsapp
-rem // usage: make_whatsapp_video.bat <video_to_convert>
-ffmpeg -i %1 -c:v libx264 -profile:v baseline -level 3.0 -pix_fmt yuv420p %~n1_converted.mp4
