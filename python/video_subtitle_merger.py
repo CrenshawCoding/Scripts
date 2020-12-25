6ed0728e4884df3f75194eb55da9ecc7886bcac5
Crenshaw <crenshawdevcontact@gmail.com>
2020-12-25 16:19:37 +0100

    Version one of python merger working

--- python/video_subtitle_merger.py
+++ python/video_subtitle_merger.py
@@ -1,9 +1,89 @@
 import argparse
+import os, sys
+import shutil
 
 # Setting up the arg parser:
 parser = argparse.ArgumentParser(description='parse for relevant information')
-parser.add_argument('video_format')
-parser.add_argument('subtitle_format')
-parser.add_argument('--dir', help='Root directory for merging')
+parser.add_argument('video_format', help='File ending of the video files, e.g. mkv, mp4, etc')
+parser.add_argument('subtitle_format', help='File ending of the subtitle files, e.g. srt')
+parser.add_argument('--dir', help='Root directory for merging, default is \'.\'', default='.')
 args = parser.parse_args()
-print(args)
+
+def colliding_files_in_dir(directory):
+    return True if count_files_of_type(args.video_format, directory) > 1 else False
+
+def count_files_of_type(file_ext, directory):
+    counter = 0
+    for f in os.listdir(directory):
+        if(f.endswith('.' + file_ext)):
+            counter = counter + 1
+    return counter
+    
+def get_all_files_of_type(file_ext, directory):
+    file_list = []
+    for f in os.listdir(directory):
+        if(f.endswith('.' + file_ext)):
+            file_list.append(f)
+    return file_list
+    
+    
+#Script start:
+  
+inp = False  
+for root, dirs, files in os.walk(args.dir, topdown = False):
+    if root == args.dir: #skipping the top level. Checks for this are done seperately later
+        continue
+    
+    for name in files:
+        if name.endswith('.' + args.video_format) and os.path.join(root, name).count('/') > 2: #found video files in subsubdirectories of root
+            print('The root directory \'' + str(args.dir) + '\' is flawed. It may only contain video or subtitle files in subdirectories of \'' + str(args.dir) + '\' but \'' + str(os.path.join(root, name)) + '\' was found.')
+            if not inp:
+                inp = raw_input('Do you wish to move all of them to the root dir' + os.path.join(args.dir, name) + ' automatically? Type \'y\' or \'n\'\n')
+            if inp == 'y':  #moving the file to the root directory
+                shutil.move(os.path.join(root, name), os.path.join(args.dir, name))
+                print('Moved', os.path.join(root, name), 'to', os.path.join(args.dir, name))
+            elif inp == 'n':
+                print('Exiting.')
+                sys.exit()
+    
+    for name in dirs:
+        if count_files_of_type(args.video_format, os.path.join(root, name)) > 2: #found multiple video files in a subdirectory
+            print('Found multiple video files in \'' + os.path.join(root, name) + '\'')
+            print('Exiting.')
+
+
+#Multiple files of same type in base directory, offer to sort them
+if(colliding_files_in_dir(args.dir)):
+    inp = raw_input('Multiple files with the same extension in root detected\nDo you wish to sort them into subdirectories automatically? Type \'y\' or \'n\':\n')
+    if inp == 'y':
+        all_video_files = get_all_files_of_type(args.video_format, args.dir)
+        all_subtitle_files = get_all_files_of_type(args.subtitle_format, args.dir)
+        for x in range(0, len(all_video_files)):
+            try:
+                dirName = args.dir + os.sep + str(x + 1)
+                print('creating', dirName)
+                os.mkdir(dirName)
+            except OSError as e:
+                if e.errno == errno.EEXist:
+                    print('directory', dirName, 'already exists.')
+            target = dirName + os.sep + all_video_files[x]
+            print('moving', all_video_files[x], 'to', target)
+            shutil.move(all_video_files[x], target)
+            if x < len(all_subtitle_files):
+                target = dirName + os.sep + all_subtitle_files[x]
+                print('moving', all_subtitle_files[x], 'to', target)
+                shutil.move(all_subtitle_files[x], target)
+            
+    elif inp == 'n':
+        print('Try again after manually sorting the files into subdirectories')
+        sys.exit()
+
+for root, dirs, files in os.walk(args.dir, topdown = True):
+    for name in dirs:
+        if count_files_of_type(args.video_format, os.path.join(root, name)) == 1 and count_files_of_type(args.subtitle_format, os.path.join(root, name)) > 0:
+            video_file = get_all_files_of_type(args.video_format, os.path.join(root, name))[0]
+            merge_command = 'mkvmerge ' + '-o ' + os.path.join(args.dir, video_file) + ' ' + os.path.join(root, name, video_file) + ' '
+            for subfile in get_all_files_of_type(args.subtitle_format, os.path.join(root, name)):
+                merge_command = merge_command + os.path.join(root, name, subfile) + ' '
+            print(merge_command)
+            os.system(merge_command)
