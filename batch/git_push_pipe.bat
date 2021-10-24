bd62867e65e6e228baa7bd1cf44ddc3d18303bf4
CrenshawCoding <anonyesmaybe@gmail.com>
2021-10-24 14:13:21 +0200

    update von git push pipe

--- batch/git_push_pipe.bat
+++ batch/git_push_pipe.bat
@@ -1,5 +1,5 @@
 @echo off
-set /p files_to_add=Welche Dateien willst du hinzufügen?:
+set /p files_to_add=Welche Dateien willst du hinzufuegen?:
 set /p commit_message=Was ist die commit message?:
 git add %files_to_add%
 git commit -m "%commit_message%"
